6a946eb4c0e79ab0c085454010eb80a7c29b83b8
mouseylicense <nevobloch@gmail.com>
2024-08-01 23:51:55 +0300

    finished filtering functionallity and started designing

--- static/filters.js
+++ static/filters.js
@@ -4,14 +4,19 @@ const teacher = document.getElementById('teacher-select');
 
 
 function filterTeachers(){
-    for(let i=0; i < classes.length; i++){
+    console.log(teacher.value);
+    if(!(teacher.value === '')){
+    for(let i=0; i < classes.length; i++) {
 
-        if(!(classes[i].dataset.teacher === teacher.value)){
-            classes[i].hidden = true;
-            console.log("true")
+        if (!(classes[i].dataset.teacher === teacher.value)) {
+            classes[i].classList.add("teacherFilter");
+        } else {
+            classes[i].classList.remove('teacherFilter');
         }
-        else {
-            classes[i].hidden = false;
+    }} else {
+
+        for(let i=0; i < classes.length; i++) {
+            classes[i].classList.remove("teacherFilter");
         }
     }
 }
@@ -19,25 +24,36 @@ function filterGrades(){
     let grades = []
     for(let i=0;i<checkboxes.length;i++){
         if(checkboxes[i].checked){
-            grades.push(checkboxes[i].value);
+            grades.push(parseInt(checkboxes[i].value));
         }
     }
     if(grades.length > 0){
     console.log(grades)
     for(let i=0;i<classes.length;i++){
+        console.log("run")
         for(let j=0;j<grades.length;j++){
-            console.log(classes[i].dataset.all_grades.includes(grades[j]));
-            if(!(classes[i].dataset.all_grades.includes(grades[j]))){
-                classes[i].hidden = true;
+            console.log(classes[i].dataset.all_grades);
+            console.log(JSON.parse(classes[i].dataset.all_grades));
+            if(JSON.parse(classes[i].dataset.all_grades).indexOf(grades[j])){
+                classes[i].classList.add("filterGrade");
             }
             else {
-                classes[i].hidden = false;
+                classes[i].classList.remove("filterGrade");
             }
         }
     }}
     else {
         for(let i=0;i<classes.length;i++){
-            classes[i].hidden =false
+            classes[i].classList.remove("filterGrade");
         }
     }
+}
+function uncheck(){
+    for(const checkbox of checkboxes){
+        if(checkbox.checked){
+            checkbox.checked=false
+
+        }
+    }
+    filterGrades()
 }
