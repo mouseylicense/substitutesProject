994878010a78800e438ac51f7d6acd7c0dd041cf
mouseylicense <nevobloch@gmail.com>
2024-09-03 22:58:47 +0300

    Removed all migrations and made one new one, edited docker compose version and changed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 python3 manage.py collectstatic --noinput
 # i commit my migration files to git so i dont need to run it on server
 # ./manage.py makemigrations app_name
-python3 manage.py migrate auth --noinput
-python3 manage.py migrate timetable 0001_initial
+
 python3 manage.py migrate --noinput
 
 # here it start nginx and the uwsgi
