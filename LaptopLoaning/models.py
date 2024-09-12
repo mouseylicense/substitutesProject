b20ad89e1f7a761a9c27aa80cc1bd0a34e26d805
mouseylicense <nevobloch@gmail.com>
2024-09-12 12:26:36 +0300

    Finished pin auth started working on laptop order form

--- LaptopLoaning/models.py
+++ LaptopLoaning/models.py
@@ -8,10 +8,22 @@ import timetable.models
 
 def generate_pin():
     return ''.join(str(randint(0, 9)) for _ in range(4))
+
+
 # Create your models here.
 class LaptopPin(models.Model):
     Teacher = models.ForeignKey(timetable.models.Teacher, on_delete=models.CASCADE)
     PIN = models.CharField(max_length=4, default=generate_pin, unique=True)
     date = models.DateField()
     uses = models.IntegerField(default=2)
-    numberOfLaptops = models.IntegerField(default=1,validators=[MinValueValidator(1)])
+    numberOfLaptops = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+
+    def use(self):
+        self.uses = self.uses - 1
+        if self.uses <= 0:
+            self.delete()
+        else:
+            self.save()
+
+    def __str__(self):
+        return self.PIN + self.Teacher.first_name + " " + str(self.uses)
--- LaptopLoaning/views.py
+++ LaptopLoaning/views.py
@@ -1,5 +1,27 @@
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .forms import LaptopLoaningForm
+from .models import LaptopPin
 
 # Create your views here.
+
 def check_pin(request):
-    pass
+    pin = request.GET.get('pin')
+    if LaptopPin.objects.filter(PIN=pin).exists():
+
+        pin = LaptopPin.objects.filter(PIN=pin).get()
+        if pin.date == datetime.today().date():
+            pin.use()
+            return HttpResponse(200)
+    return HttpResponseNotFound(404)
+
+@login_required
+def order_laptops(request):
+    if request.method == 'POST':
+        form = LaptopLoaningForm(request.POST)
+        if form.is_valid():
+            form.save()
+    user = request.user
+    return render(request,"order_laptops.html",{"form":LaptopLoaningForm(initial={"Teacher":user})})
