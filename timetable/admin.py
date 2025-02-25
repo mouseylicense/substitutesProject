10c49cd485e5321066d098ee8434591bc546aac2
mouseylicense <nevobloch@gmail.com>
2025-02-25 21:25:06 +0200

    Enhance ClassAdmin with day ordering and improved filtering options

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from . import models
+from django.db.models import Case, When, Value, IntegerField
+from django.contrib import admin
+
+DAY_ORDER = {
+    "Sunday": 0,
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+}
 NUMBERS_TO_GRADES = {
     0:"First_Grade",
     1:"Second_Grade",
@@ -20,7 +32,22 @@ NUMBERS_TO_GRADES = {
 
 class ClassAdmin(admin.ModelAdmin):
     fields = [('name','description'),('day_of_week','hour'),'room','teachers',('student_teacher','student_teaching'),('first_grade','second_grade','third_grade','fourth_grade','fifth_grade','sixth_grade','seventh_grade','eighth_grade','ninth_grade','tenth_grade','eleventh_grade','twelfth_grade'),('visible',)]
+    list_filter = ['day_of_week','hour','first_grade','second_grade','third_grade','fourth_grade','fifth_grade','sixth_grade','seventh_grade','eighth_grade','ninth_grade','tenth_grade','eleventh_grade','twelfth_grade']
+    list_display = ['name','day','hour','room','first_grade','second_grade','third_grade','fourth_grade','fifth_grade','sixth_grade','seventh_grade','eighth_grade','ninth_grade','tenth_grade','eleventh_grade','twelfth_grade']
+    search_fields = ['name','room__name','teachers__first_name','teachers__last_name']
+    def get_queryset(self, request):
+        """Annotate queryset with day_order before returning."""
+        queryset = super().get_queryset(request).annotate(
+            day_order=Case(
+                *[When(day_of_week=day, then=Value(order)) for day, order in DAY_ORDER.items()],
+                output_field=IntegerField()
+            )
+        )
+        return queryset
 
+    def day(self, obj):
+        return obj.day_of_week
+    day.admin_order_field = 'day_order'
 
 class TeacherAdmin(admin.ModelAdmin):
     fields = [('first_name','last_name'),('email','phone_number','slack_id'),('can_substitute','tutor','shocher'),('Sunday','Monday','Tuesday','Wednesday','Thursday'),('user_permissions','groups','is_superuser','password','is_staff','manage_schedule','manage_subs','type','last_sub')]
